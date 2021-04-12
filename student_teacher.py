cb418e581d8493b00da3f708c357ce345f910203
kybky <1587736356@qq.com>
2021-04-12 11:14:02 +0800

    first commit

---
+++ student_teacher.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+class Person(object):
+    """
+    返回具有给定名称的 Person 对象
+    """
+
+    def __init__(self, name):
+        self.name = name
+
+    def get_details(self):
+        """
+        返回包含人名的字符串
+        """
+        return self.name
+
+
+class Student(Person):
+    """
+    返回 Student 对象，采用 name, branch, year 3 个参数
+    """
+
+    def __init__(self, name, branch, year):
+        Person.__init__(self, name)
+        self.branch = branch
+        self.year = year
+
+    def get_details(self):
+        """
+        返回包含学生具体信息的字符串
+        """
+        return "{} studies {} and is in {} year.".format(self.name, self.branch, self.year)
+
+
+class Teacher(Person):
+    """
+    返回 Teacher 对象，采用字符串列表作为参数
+    """
+    def __init__(self, name, papers):
+        Person.__init__(self, name)
+        self.papers = papers
+
+    def get_details(self):
+        return "{} teaches {}".format(self.name, ','.join(self.papers))
+
+
+person1 = Person('Sachin')
+student1 = Student('Kushal', 'CSE', 2005)
+teacher1 = Teacher('Prashad', ['C', 'C++'])
+
+print(person1.get_details())
+print(student1.get_details())
+print(teacher1.get_details())  
+
+#类 ，继承  
+
+#在这个例子中你能看到我们是怎样在 Student 类和 Teacher 类中调用 Person 类的 __init__ 方法。
+
+#我们也在 Student 类和 Teacher 类中重写了 Person 类的 get_details() 方法。
+
+#因此，当我们调用 student1 和 teacher1 的 get_details() 方法时，使用的是各自类（Student 和 Teacher）中定义的方法。
